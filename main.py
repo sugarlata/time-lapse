14abee1931cdca1c27ae77ff8201230e63fa1412
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2019-03-31 16:13:35 +1100

    Skeleton Code

---
+++ main.py
@@ -0,0 +1,30 @@
+import cv2
+
+
+def get_image(img_loc, img_name):
+    pass
+
+
+def time_lapse_loop(sunset):
+    pass
+
+def get_sunrise():
+    pass
+
+def get_sunset():
+    pass
+
+
+def begin_day(period=3):
+    pass
+    # Get sunrise / sunset
+    # Sleep until 10 mins before sunrise
+    # Begin time lapse loop
+    # Combine into Video
+    # Upload video
+
+
+
+if __name__ == '__main__':
+
+    begin_day()

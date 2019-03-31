fbb26fb3a823fbb6920b24d37a85918f31bbf513
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2019-03-31 19:27:05 +1100

    Ready for Dist

--- uploader.py
+++ uploader.py
@@ -1,6 +1,29 @@
+import os
+import main
+import arrow
+import datetime
 import youtube_upload
 
-VIDEO_FN = '/Volumes/SW/drop/raw.mp4'
+ARCHIVE_LOCATION = main.ARCHIVE_LOCATION
 
-youtube_upload.upload(VIDEO_FN, 'Christmas Supercell Refined and Interpolated', """Radar grabs from Melbourne 2011-12-25
-Tornado producing supercell was isolated and interpolated to view radar scan.""", '28', '', 'public')
+
+def upload_to_youtube(days_back=1):
+    # Check if file exists
+    yestdy = arrow.now() - datetime.timedelta(days=days_back)
+    loc = os.path.join(ARCHIVE_LOCATION, str(yestdy.year), str(yestdy.month), str(yestdy.day))
+    dt = str(yestdy.format('YYYY-MM-DD'))
+    mp4_fn = '.mp4' % dt
+
+    vid_fn = os.path.join(loc, mp4_fn)
+
+    if os.path.exists(vid_fn):
+        sun_times = main.get_sun_times(yestdy)
+        start_time = (sun_times['dawn'] - datetime.timedelta(minutes=10)).format('DD/MM/YYYY HH:mm')
+        finish_time = (sun_times['dusk'] + datetime.timedelta(minutes=10)).format('DD/MM/YYYY HH:mm')
+        resp = youtube_upload.upload(vid_fn, 'Doncaster Time Lapse %s' % dt, """Time lapse from Doncaster, Victoria, Australia.
+Taken looking NW. 1 Frame / 3 seconds. 25fps.
+Date: %s
+Start time: %s
+Finish time: %s""" % (dt, start_time.format('HH:mm'), finish_time.format('HH:mm')), '28', '', 'public')
+
+        print resp

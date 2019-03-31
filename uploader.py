4fe5c2dadda5cd6325f9ab8c429b95560d6301d2
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2019-03-31 18:50:47 +1100

    Youtube Upload Portion

---
+++ uploader.py
@@ -0,0 +1,6 @@
+import youtube_upload
+
+VIDEO_FN = '/Volumes/SW/drop/raw.mp4'
+
+youtube_upload.upload(VIDEO_FN, 'Christmas Supercell Refined and Interpolated', """Radar grabs from Melbourne 2011-12-25
+Tornado producing supercell was isolated and interpolated to view radar scan.""", '28', '', 'public')

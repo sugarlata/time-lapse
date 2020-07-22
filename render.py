70c3a282df260c19a452a95ad14d29b23456c09c
sugarlata <nathan.sgarlata@gmail.com>
2020-07-23 00:30:54 +1000

    Adds render module

---
+++ render.py
@@ -0,0 +1,20 @@
+import os
+import config
+import arrow
+
+
+def render_video(shift_yesterday=False):
+    if shift_yesterday:
+        yesterday = arrow.now().shift(days=-1)
+    else:
+        yesterday = arrow.now()
+    
+    vid_name = str(yesterday.format('YYYY-MM-DD'))
+        
+    os.chdir(os.path.join(
+        config.Misc.archive_location,
+        str(yesterday.year),
+        str(yesterday.month),
+        str(yesterday.day)))
+
+    os.system(config.Misc.cmd % vid_name)
